c143e4ad41dbf38bd271cdac795c885e7fa0eced
Sander Mertens <sander.mertens8@gmail.com>
2020-09-12 02:15:25 -0700

    Add support for C++

--- include/flecs_components_gui.h
+++ include/flecs_components_gui.h
@@ -48,4 +48,70 @@ void FlecsComponentsGuiImport(
 }
 #endif
 
+#ifdef __cplusplus
+
+namespace flecs {
+namespace components {
+
+class gui : FlecsComponentsGui {
+public:
+    struct Window : EcsWindow {
+        Window() {
+            this->title = nullptr;
+            this->x = 0;
+            this->y = 0;
+            this->width = 0;
+            this->height = 0;
+        }
+    };
+
+    using Canvas = EcsCanvas;
+
+    struct Camera : EcsCamera {
+        Camera() {
+            this->set_position(0, 0, 0);
+            this->set_lookat(0, 1, 1);
+            this->set_up(0, -1, 0);
+            this->set_fov(30);
+        }
+
+        void set_position(float x, float y, float z) {
+            this->position[0] = x;
+            this->position[1] = y;
+            this->position[2] = z;
+        }
+
+        void set_lookat(float x, float y, float z) {
+            this->lookat[0] = x;
+            this->lookat[1] = y;
+            this->lookat[2] = z;
+        }
+
+        void set_up(float x, float y, float z) {
+            this->up[0] = x;
+            this->up[1] = y;
+            this->up[2] = z;
+        }
+
+        void set_fov(float value) {
+            this->fov = value;
+        }
+    };
+
+    gui(flecs::world& ecs) {
+        FlecsComponentsGuiImport(ecs.c_ptr());
+
+        ecs.module<flecs::components::gui>();
+
+        ecs.pod_component<Window>("flecs::components::gui::Window");
+        ecs.pod_component<Canvas>("flecs::components::gui::Canvas");
+        ecs.pod_component<Camera>("flecs::components::gui::Camera");
+    }
+};
+
+}
+}
+
+#endif
+
 #endif
